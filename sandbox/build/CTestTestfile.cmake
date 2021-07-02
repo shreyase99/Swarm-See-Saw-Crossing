002a6ad31c963f95ffb8469e53d4733f9d2b857c
Shreyase <31140592+shreyase99@users.noreply.github.com>
2021-07-03 00:53:10 +0530

    Delete sandbox directory

--- sandbox/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pranav/repository.2/sandbox/src
-# Build directory: /home/pranav/repository.2/sandbox/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("learning_tf")
